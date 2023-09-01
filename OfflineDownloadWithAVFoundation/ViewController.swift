53274e54e73c4a715a0b917e234b341d80bc2bb6
hari-testpress <hari@testpress.in>
2023-09-01 13:49:41 +0530

    Setup player with non-DRM video

--- OfflineDownloadWithAVFoundation/ViewController.swift
+++ OfflineDownloadWithAVFoundation/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var playerContainer: UIView!
+    var playBackURL = URL(string: "https://d384padtbeqfgy.cloudfront.net/transcoded/8r65J7EY6NP/video.m3u8")!
+    var player: AVPlayer?
+    var playerViewController: AVPlayerViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupPlayerView()
     }
+    
+    func setupPlayerView(){
+        player = AVPlayer(url: playBackURL)
+        playerViewController = AVPlayerViewController()
+        playerViewController?.player = player
 
-
+        addChild(playerViewController!)
+        playerContainer.addSubview(playerViewController!.view)
+        playerViewController!.view.frame = playerContainer.bounds
+    }
 }
 
